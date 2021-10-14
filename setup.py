1b87e2f9f545cc5de4ab52cc3a0af860abf42b11
Arraying <paul.huebner@googlemail.com>
2021-10-14 13:07:55 +0200

    Convert into Python package.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="andy_fraud",
+    version="0.1.4",
+    author="Arraying",
+    author_email="paul.huebner@googlemail.com",
+    description="Anti-typosquatting suite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arraying/Andy",
+    project_urls={
+        "Bug Tracker": "https://github.com/Arraying/Andy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=["andy"],
+    install_requires=[
+        "tldextract",
+        "jsonschema"
+    ],
+    python_requires=">=3.8",
+)
